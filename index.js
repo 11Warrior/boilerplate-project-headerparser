695b3d7b1b9d6ffdfcf3c78b6416622cd30f4cc5
Aayush <aayushthapa640@gmail.com>
2025-05-01 23:09:14 +0530

    Second Project

--- index.js
+++ index.js
@@ -23,8 +23,22 @@ app.get('/', function (req, res) {
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
+app.set('trust proxy', true); // Trust the reverse proxy
+
+app.get('/api/whoami', (req, res) => {
+  const ip = req.headers['x-forwarded-for']?.split(',')[0] || req.ip;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+
+  res.json({
+    ipaddress: ip,
+    language: language,
+    software: software
+  });
+});
+
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || 5000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
